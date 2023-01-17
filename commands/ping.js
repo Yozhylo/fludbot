8eb041ea91b33dbd0fbc595f56219218b1dced45
unknown <N/A>
2023-01-17 02:35:43 +0200

    Port to github

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,10 @@
-const { SlashCommandBuilder, Locale, WebSocketManager, WebSocketShard} = require("discord.js");
+const { SlashCommandBuilder, Locale, User} = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
   async execute(interaction) {
+  console.log(`${User.username} called /ping`)
   await interaction.reply({content: 'Pong!', ephemeral: true});
   },
 };
