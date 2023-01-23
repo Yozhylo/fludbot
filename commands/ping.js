1048f251926fbaa82eda44feb5dbded4710cc899
unknown <N/A>
2023-01-23 17:26:32 +0200

    Working on proper DB integration

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,20 @@
-const { SlashCommandBuilder, Locale, User} = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
+
+// module.exports = {
+//   data: new SlashCommandBuilder()
+//         .setName('ping')
+//         .setDescription('Replies with Pong!'),
+//   async execute(interaction) {
+//     // console.log(`${interaction.user.username} called /ping`)
+//     await interaction.reply('test');
+//   },
+// };
 
 module.exports = {
-  data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
-  async execute(interaction) {
-  console.log(`${User.username} called /ping`)
-  await interaction.reply({content: 'Pong!', ephemeral: true});
-  },
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
 };
