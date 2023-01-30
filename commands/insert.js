fd6bc61af086911e44c860759711cfb761f846ba
Yozhylo <polidar2012@gmail.com>
2023-01-30 20:26:11 +0200

    /insert: added comments

--- commands/insert.js
+++ commands/insert.js
@@ -28,15 +28,19 @@ module.exports = {
       .setMaxLength(255)),
     async execute(interaction) {
       await interaction.deferReply({ephemeral: true});
+      
+      // Variables with file data
       let options = interaction.options,
       nick = options.getString('nickname'),
       info = options.getAttachment('media'),
-      description = options.getString('Description');
-      ext = new Extension(path.extname(info.name));
-
-      let file = 
-      new Media(info.url, nick, info.name, description, '', ext);
+      description = options.getString('Description'),
+      ext = new Extension(path.extname(info.name)),
+      // Init a file variable
+      file = new Media(info.url, nick, info.name, description, '', ext);
+      
       console.log(`${interaction.user.username} inserts data to DB\n`, await interaction.options.resolved);
+      
+      // Downloading and inserting data
       await download(info.url, info.name);
       await db.insert(file.reference, file.name, file.nickname, file.description, file.extension.format);
       await interaction.editReply('Download finished');
