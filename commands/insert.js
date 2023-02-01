ac9834cacde0156ed0fd101f9c1acee9324443cb
Yozhylo <polidar2012@gmail.com>
2023-02-01 14:32:16 +0200

    /insert: commented and fixed a small bug

--- commands/insert.js
+++ commands/insert.js
@@ -5,44 +5,45 @@ const path = require('node:path');
 
 const { DB } = require("../classes/db.js");
 const credentials = require("../.data-base/config.json");
-const db = new DB(credentials);
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('insert')
-    .setDescription('Inserts data into database')
-    .addAttachmentOption( option => option
-      .setName('media')
-      .setDescription('The file to be uploaded')
-      .setRequired(true))
+  .setName('insert')
+  .setDescription('Inserts data into database')
+  .addAttachmentOption( option => option
+    .setName('media')
+    .setDescription('The file to be uploaded')
+    .setRequired(true))
     .addStringOption( option => option
       .setName('nickname')
       .setDescription('The nickname for the file to be retrived by.')
       .setRequired(true)
       .setMinLength(1)
       .setMaxLength(32))
-    .addStringOption( option => option
-      .setName('description')
-      .setDescription('Description of the file')
-      .setRequired(false)
-      .setMaxLength(255)),
-    async execute(interaction) {
-      await interaction.deferReply({ephemeral: true});
-      
-      // Variables with file data
-      let options = interaction.options,
-      nick = options.getString('nickname'),
-      info = options.getAttachment('media'),
-      description = options.getString('Description'),
-      ext = new Extension(path.extname(info.name)),
-      // Init a file variable
-      file = new Media(info.url, nick, info.name, description, '', ext);
-      
-      console.log(`${interaction.user.username} inserts data to DB\n`, await interaction.options.resolved);
-      
-      // Downloading and inserting data
-      await download(info.url, info.name);
-      await db.insert(file.reference, file.name, file.nickname, file.description, file.extension.format);
-      await interaction.editReply('Download finished');
-    }
+      .addStringOption( option => option
+        .setName('description')
+        .setDescription('Description of the file')
+        .setRequired(false)
+        .setMaxLength(255)),
+  async execute(interaction) {
+    await interaction.deferReply({ephemeral: true});
+
+    const db = new DB(credentials),
+    // Variables with file data
+    options = interaction.options,
+    nick = options.getString('nickname'),
+    info = options.getAttachment('media'),
+    description = options.getString('Description'),
+    ext = new Extension(path.extname(info.name)),
+    // Init a file variable
+    file = new Media(info.url, nick, info.name, description, '', ext);
+    
+    console.log(`${interaction.user.username} inserts data to DB\n`, await interaction.options.resolved);
+    // Downloading and inserting data
+    // !!!Need to implement callback here!!!
+    await download(info.url, path.basename(info.name));
+    await db.insert(file.reference, file.name, file.nickname, file.description, file.extension.format);
+    await db.close();
+    await interaction.editReply('Download finished');
+  }
 }
