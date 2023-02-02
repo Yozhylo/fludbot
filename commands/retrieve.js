148781c4becd658ff331a5adc22ede77eb85b4cd
Yozhylo <polidar2012@gmail.com>
2023-02-02 12:32:27 +0200

    /retreive: final command version

--- commands/retrieve.js
+++ commands/retrieve.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder } = require('discord.js');
 
 const { DB } = require("../classes/db.js");
 const credentials = require("../.data-base/config.json");
+const { Media } = require('../classes/classes.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -16,10 +17,11 @@ module.exports = {
     async execute(interaction) {
       await interaction.deferReply();
 
-      const db = new DB(credentials);
+      const db = new DB(credentials),
+      fileNick = interaction.options.getString('file-nickname');
       // Hacky way of passing a callback
       // !!!MOVE TO FUNCTION LATER!!!
-      db.retrieve(interaction.options.getString('file-nickname'), (err, result) => {
+      db.retrieve(fileNick, async (err, result) => {
         // Checking for errors and for empty set
         if(err) {
           console.error(err)
@@ -28,12 +30,27 @@ module.exports = {
         else if(result === undefined) interaction.editReply('Requested asset does not exist!');
         // Retrieving data, based on DB
         else {
-        const storage = path.join('..','.data-base','file-storage','/');
-        console.log(storage+result.Name);
-        // const file = new AttachmentBuilder(storage+result[Name]);
+          let fileDesc;
+          result.Description === 'null' || '' ? fileDesc = 'No description provided.' : fileDesc = result.Description;
+          const file = path.join('.data-base','file-storage','/', result.Name + result.Format),
+          media = new AttachmentBuilder(file),
+          // const embed = new EmbedBuilder()
+          //   .setTitle(`${fileNick}`)
+          //   .setImage(`attachment://${path.basename(file)}`)
+          //   .setDescription(`${result.Description}`);
+          embed = new EmbedBuilder()
+          .setTitle(`Nickname: ${fileNick}`)
+          // .setImage(`attachment://${path.basename(file)}`)
+          .setDescription(`Description: ${fileDesc}`);
+          // await interaction.channel.send({ embeds: [embed], files: [media] });
+          // await interaction.channel.send({ embeds: [embed] });
+          // await interaction.channel.send( {files: [media]} );
+          // await interaction.followUp( {embeds: [embed]} );
+          await interaction.editReply( {content: 'File retrieved:', embeds: [embed]} );
+          await interaction.followUp( {files: [media]} );
         }
       });
       await db.close();
-      await interaction.editReply('DONE')
+
   }
 }
