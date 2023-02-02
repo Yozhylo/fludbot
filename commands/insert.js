eb547484d295e0f4dffafb44398897d85226daaa
Yozhylo <polidar2012@gmail.com>
2023-02-02 12:30:27 +0200

    /insert: cleaned up code a bit, and slightly changed inserted data

--- commands/insert.js
+++ commands/insert.js
@@ -14,17 +14,17 @@ module.exports = {
     .setName('media')
     .setDescription('The file to be uploaded')
     .setRequired(true))
-    .addStringOption( option => option
-      .setName('nickname')
-      .setDescription('The nickname for the file to be retrived by.')
-      .setRequired(true)
-      .setMinLength(1)
-      .setMaxLength(32))
-      .addStringOption( option => option
-        .setName('description')
-        .setDescription('Description of the file')
-        .setRequired(false)
-        .setMaxLength(255)),
+  .addStringOption( option => option
+    .setName('nickname')
+    .setDescription('The nickname for the file to be retrived by.')
+    .setRequired(true)
+    .setMinLength(1)
+    .setMaxLength(32))
+  .addStringOption( option => option
+    .setName('description')
+    .setDescription('Description of the file')
+    .setRequired(false)
+    .setMaxLength(255)),
   async execute(interaction) {
     await interaction.deferReply({ephemeral: true});
 
@@ -33,17 +33,18 @@ module.exports = {
     options = interaction.options,
     nick = options.getString('nickname'),
     info = options.getAttachment('media'),
-    description = options.getString('Description'),
+    description = options.getString('description'),
     ext = new Extension(path.extname(info.name)),
     // Init a file variable
-    file = new Media(info.url, nick, info.name, description, '', ext);
+    file = new Media(info.url, nick, path.basename(info.name).replace(/\.\w+$/,''), description, '', ext);
     
     console.log(`${interaction.user.username} inserts data to DB\n`, await interaction.options.resolved);
     // Downloading and inserting data
     // !!!Need to implement callback here!!!
-    await download(info.url, path.basename(info.name));
+    await download(info.url, info.name);
     await db.insert(file.reference, file.name, file.nickname, file.description, file.extension.format);
     await db.close();
+
     await interaction.editReply('Download finished');
   }
 }
